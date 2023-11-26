aa0739cee78fab0e51a9d9f3b22f88a1cc7b93f0
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-11-27 03:14:39 +0530

    all basics done

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,105 @@
+//TODO turn pageswhen click next or prev button
+const pageTurnBtn = document.querySelectorAll(".next-prev-btn");
+// console.log(pageTurnBtn);
+
+pageTurnBtn.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    const pageTurnId = el.getAttribute("data-page");
+    const pageTurn = document.getElementById(pageTurnId);
+
+    if (pageTurn.classList.contains("turn")) {
+      pageTurn.classList.remove("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 - index;
+      }, 500);
+    } else {
+      pageTurn.classList.add("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 + index;
+      }, 500);
+    }
+  });
+});
+
+//TODO contact me button when clicked
+const pages = document.querySelectorAll(".book-page.page-right");
+const contactMeBtn = document.querySelector(".btn.contact-me");
+console.log(pages, contactMeBtn);
+
+contactMeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add("turn");
+
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+});
+
+// TODO create reverse index function
+
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = totalPages - 1;
+  }
+}
+
+const backProfileBtn = document.querySelector(".back-profile");
+
+backProfileBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  pages.forEach((_, index) => {
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].classList.remove("turn");
+
+      setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+});
+
+//TODO opening animation
+
+const coverRight = document.querySelector(".cover.cover-right");
+const pageLeft = document.querySelector(".book-page.page-left");
+
+//opening animation (cover right animation);
+setTimeout(() => {
+  coverRight.classList.add("turn");
+}, 2100);
+
+setTimeout(() => {
+  coverRight.style.zIndex = -1;
+}, 2800);
+
+//opening animation (all pages left animation);
+setTimeout(() => {
+  pageLeft.style.zIndex = 20;
+}, 3200);
+
+//opening animation (all pages right animation);
+pages.forEach((_, index) => {
+  setTimeout(() => {
+    reverseIndex();
+    pages[pageNumber].classList.remove("turn");
+
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].style.zIndex = 10 + index;
+    }, 500);
+  }, (index + 1) * 200 + 2100);
+});
+
 // TODO Cursor Animation
 let script = document.currentScript;
 window.addEventListener("DOMContentLoaded", () => {
@@ -182,3 +284,5 @@ window.addEventListener("DOMContentLoaded", () => {
     document.ontouchmove = moove;
   }
 });
+
+//TODO Email sent
