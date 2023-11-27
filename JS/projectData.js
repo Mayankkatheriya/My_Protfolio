eabbdbae6e463b5161932367e1b3e9943476efe2
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-11-27 17:24:59 +0530

    project page added

---
+++ JS/projectData.js
@@ -0,0 +1,131 @@
+export const projectData = [
+    {
+        name : "Amazon Clone",
+        hosted : "https://mayankkatheriya.github.io/Amazon-clone/",
+        github : "https://github.com/Mayankkatheriya/Amazon-clone.git",
+        type : "CSS",
+        img : "./Assets/amazon.gif",
+        top : false
+
+    },
+    {
+        name : "Animated Login Page",
+        hosted : "https://mayankkatheriya.github.io/Mini-Project-1-Animated_Login_Page/",
+        github : "https://github.com/Mayankkatheriya/Mini-Project-1-Animated_Login_Page.git",
+        type : "CSS",
+        img : "./Assets/login-page.png",
+        top : true
+    },
+    {
+        name : "Landing Page - 1",
+        hosted : "https://mayankkatheriya.github.io/Weekly_Test_6-CSS/",
+        github : "https://github.com/Mayankkatheriya/Weekly_Test_6-CSS.git",
+        type : "CSS",
+        img : "./Assets/landing-page-1.png",
+        top : false
+    },
+    {
+        name : "Animation",
+        hosted : "https://mayankkatheriya.github.io/Assignment_10-Animations/",
+        github : "https://github.com/Mayankkatheriya/Assignment_10-Animations.git",
+        type : "CSS",
+        img : "./Assets/Animations.png",
+        top : true
+    },
+    {
+        name : "Tailwind Landing Page",
+        hosted : "https://mayankkatheriya.github.io/Major_Project_using_Tailwind/dist/",
+        github : "https://github.com/Mayankkatheriya/Major_Project_using_Tailwind.git",
+        type : "tailwind",
+        img : "./Assets/tailwind-landing-page.png",
+        top : false
+    },
+    {
+        name : "Youtube Clone",
+        hosted : "https://mayankkatheriya.github.io/Assignment_8-YouTube_UI/",
+        github : "https://github.com/Mayankkatheriya/Assignment_8-YouTube_UI.git",
+        type : "CSS",
+        img : "./Assets/youtube-clone.png",
+        top : false
+    },
+    {
+        name : "3D Waves",
+        hosted : "https://mayankkatheriya.github.io/3D_waves/",
+        github : "https://github.com/Mayankkatheriya/3D_waves.git",
+        type : "CSS",
+        img : "./Assets/3d-waves.png",
+        top : true
+    },
+    {
+        name : "Business landing Page",
+        hosted : "https://mayankkatheriya.github.io/Major-Project-CSS/",
+        github : "https://github.com/Mayankkatheriya/Major-Project-CSS.git",
+        type : "CSS",
+        img : "./Assets/business-landing-page.png",
+        top : true
+    },
+    {
+        name : "BookMyShow Clone",
+        hosted : "https://mayankkatheriya.github.io/BookMyShow-Clone/",
+        github : "https://github.com/Mayankkatheriya/BookMyShow-Clone.git",
+        type : "CSS",
+        img : "./Assets/BookmyShow.png",
+        top : true
+    },
+    {
+        name : "Photo Gallery CSS",
+        hosted : "https://mayankkatheriya.github.io/Photo-gallery_CSS/",
+        github : "https://github.com/Mayankkatheriya/Photo-gallery_CSS.git",
+        type : "CSS",
+        img : "./Assets/photo-gallery.png",
+        top : true
+    },
+    {
+        name : "Light Bulb",
+        hosted : "https://mayankkatheriya.github.io/Light_Bulb_JS/",
+        github : "https://github.com/Mayankkatheriya/Light_Bulb_JS.git",
+        type : "JS",
+        img : "./Assets/light_bulb.png",
+        top : true
+    },
+    {
+        name : "Mouse Position",
+        hosted : "https://mayankkatheriya.github.io/Weekly_Test-2-Mouse-Event-/",
+        github : "https://github.com/Mayankkatheriya/Weekly_Test-2-Mouse-Event-.git",
+        type : "JS",
+        img : "./Assets/mouse-position.png",
+        top : false
+    },
+    {
+        name : "Change Shape and Color",
+        hosted : "https://mayankkatheriya.github.io/Change_Shape_and_Color/",
+        github : "https://github.com/Mayankkatheriya/Change_Shape_and_Color.git",
+        type : "JS",
+        img : "./Assets/chnage_shape_color.png",
+        top : false
+    },
+    {
+        name : "Background Change",
+        hosted : "https://mayankkatheriya.github.io/Background_Color_Change/",
+        github : "https://github.com/Mayankkatheriya/Background_Color_Change.git",
+        type : "JS",
+        img : "./Assets/background-change.png",
+        top : false
+    },
+    {
+        name : "Shopping Cart",
+        hosted : "https://mayankkatheriya.github.io/Weekly_Test_3_Shopping_Cart/",
+        github : "https://github.com/Mayankkatheriya/Weekly_Test_3_Shopping_Cart.git",
+        type : "JS",
+        img : "./Assets/shopping-cart.png",
+        top : true
+    },
+    {
+        name : "Random color Code",
+        hosted : "https://mayankkatheriya.github.io/Random_Color-Code/",
+        github : "https://github.com/Mayankkatheriya/Random_Color-Code.git",
+        type : "JS",
+        img : "./Assets/random-color.png",
+        top : false
+    },
+]
---
+++ JS/projects.js
@@ -0,0 +1,1 @@
+import { projectData } from "./projectData.js";
